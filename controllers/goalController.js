08d56f8fda0c2d2ebb4c4ef77d1b66b356a1265e
wyu6609 <wy427@nyu.edu>
2022-08-16 14:50:09 -0400

    added controller functions

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -21,6 +21,8 @@ export const setGoal = asyncHandler(async (request, response) => {
     response.status(400);
     throw new Error("Please add a textfield");
   }
+  //create goals
+  const goals = await Goal.create();
 
   response.status(200).json({ message: "set goal" });
 });
@@ -29,6 +31,21 @@ export const setGoal = asyncHandler(async (request, response) => {
 // @route PUT /api/goals/:id
 // @access Private
 export const updateGoal = asyncHandler(async (request, response) => {
+  const goal = await Goal.findById(request.params.id);
+
+  if (!goal) {
+    response.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(
+    request.params.id,
+    request.body,
+    {
+      new: true,
+    }
+  );
+
   response.status(200).json({ message: "Update goal" });
 });
 
@@ -36,5 +53,14 @@ export const updateGoal = asyncHandler(async (request, response) => {
 // @route DELETE /api/goals/:id
 // @access Private
 export const deleteGoal = asyncHandler(async (request, response) => {
-  response.status(200).json({ message: "Delete goal" });
+  const goal = await Goal.findById(request.params.id);
+
+  if (!goal) {
+    response.status(400);
+    throw new Error("goal not found");
+  }
+
+  await goal.remove();
+
+  response.status(200).json({ id: request.params.id });
 });
