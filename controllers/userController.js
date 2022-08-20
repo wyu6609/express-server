10a3d7e68287155f5f6ecd1c574426389ff7a018
wyu6609 <wy427@nyu.edu>
2022-08-20 19:03:28 -0400

    register user controller update

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,56 @@ import asyncHandler from "express-async-handler";
 // @router POST /api/users
 // @access PUBLIC
 
-const registerUser = asyncHandler(async (request, response) => {});
-const loginUser = asyncHandler(async (request, response) => {});
+export const registerUser = asyncHandler(async (request, response) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  //check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+//@desc Authenticate a user
+// @route POST /api/users/login
+// @access public
+
+export const loginUser = asyncHandler(async (request, response) => {});
+
+// @desc Get user data
+// @route GET /api/users/me
+// @access private
+
 const getMe = asyncHandler(async (request, response) => {});
 
 //Generate JWT
